99651d4ff9ea281aa96c05122b6df1d6c2a313ac
Victory Imadi <omojoimadi711@gmail.com>
2026-01-13 10:38:36 +0530

    Initial commit

---
+++ EXP-1/README.md
@@ -0,0 +1,4 @@
+##Learning Outcomes
+- I learnt how to create a react application.
+- I learnt the the structure of vite.
+- I learnt how to build and run a basic single-page application.
---
+++ EXP-1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SinglePageApp from './components/Spa'
+
+function App() {
+
+  return (
+    <>
+      <SinglePageApp />
+    </>
+  )
+}
+
+export default App
